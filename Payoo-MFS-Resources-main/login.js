985ca48b63c5228054430c9b2644e31aefa5b061
shazzadul-islam-samim <Shazzadulislam090@gamil.com>
2025-01-28 14:56:59 +0600

    second commit

---
+++ Payoo-MFS-Resources-main/login.js
@@ -0,0 +1,18 @@
+// console.log('have a good day')
+document.getElementById('login-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    // console.log('hello hello')
+    const phonenumber = document.getElementById('phone-number').value;
+    // console.log(phonenumber.value);
+    const pinnumber=document.getElementById('pin-number').value;
+    console.log(phonenumber, pinnumber);
+
+    if(phonenumber==='5' && pinnumber==='1234'){
+        console.log('Thank you sir');
+        window.location.href='home.html';
+    }
+    else{
+        console.log('You are wrong.......')
+    }
+    
+})
