0967a434bad42fcd644353975c7599b706b9906a
shazzadul-islam-samim <Shazzadulislam090@gamil.com>
2025-02-01 20:59:39 +0600

    add money

--- mfs function/services.js
+++ mfs function/services.js
@@ -1,1 +1,19 @@
-console.log('services');
+// console.log('services');
+document.getElementById('btn-update-addmoney').addEventListener('click', function (event){
+    // console.log('hi');
+    event.preventDefault();
+    const addmoney=getInputFieldValuebyId('addmoney-amount');
+    const pinnumber=getInputFieldValuebyId('addmoney-pin');
+    
+    if(pinnumber===1234){
+        const balance=getTextFieldValuebyId('account-balance');
+        const newbalance=balance+addmoney;
+
+        document.getElementById('account-balance').innerText=newbalance;
+        //console.log(newbalance)
+    }
+    else{
+        alert('Wrong PIN. Please try again');
+    }
+})
+
--- mfs function/servicesUtility.js
+++ mfs function/servicesUtility.js
@@ -1,1 +1,15 @@
-console.log('services utility');
+// console.log('services utility');
+//findout for the input value
+function getInputFieldValuebyId(id){
+    const inputValue=document.getElementById(id).value;
+    const inputnumber=parseFloat(inputValue);
+    //console.log('2number',id,inputnumber);
+    return inputnumber;
+}
+
+//find for text value
+function getTextFieldValuebyId(id){
+    const textvalue=document.getElementById(id).innerText;
+    const textnumber=parseFloat(textvalue);
+    return textnumber;
+}
